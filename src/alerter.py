f4f6670855b61377696b9c4f2715bcda7244a894
johnli01 <john.li9631@gmail.com>
2020-11-04 15:43:45 -0800

    Resolved some errors, now alerts when stock price is above price

--- src/alerter.py
+++ src/alerter.py
@@ -1,13 +1,21 @@
-import pandas
+import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
-import sys
-
-#
-ts = TimeSeries("", output_format='pandas')
-values, columns = ts.get_intraday(symbol='TSLA', interval='1min')
-
-print(values)
-
 import matplotlib.pyplot as plt
-values['1. open'].plot()
-plt.show()
+import time
+
+#LDQW7564ZBSM8GWH
+ts = TimeSeries("LDQW7564ZBSM8GWH", output_format="pandas")
+
+target_price = 450
+
+while True:
+  data, meta_data = ts.get_intraday(symbol="TSLA", interval="1min", outputsize="full")
+
+  closing_data = data["4. close"]
+  recent_price = closing_data[0]
+  if recent_price >= target_price:
+    print("ALERT | STOCK IS AT: ", recent_price, " | ALERT")
+    break
+  else:
+    print("PRICE AT: ", recent_price)
+    time.sleep(5)
